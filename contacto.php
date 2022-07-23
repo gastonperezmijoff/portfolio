7af916f9242f081121d1e3096827d6dd13c2af58
gastonperezmijoff <gmijoff@gmail.com>
2022-07-22 22:17:50 -0300

    cambios

--- contacto.php
+++ contacto.php
@@ -17,33 +17,7 @@
 
 <body id="contacto" class="d-flex flex-column h-100">
     <header class="conteniner">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-2" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-2" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-2" href="proyectos.html">Poyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active px-4 py-2" href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="cv">
-                    <a href="">Descarga mi CV<i class="fas fa-download"></i></a>
-                </div>
-            </div>
-        </nav>
+<?php include_once("menu.php"); ?>
     </header>
     </body>
     <main class="container">
--- proyectos.php
+++ proyectos.php
@@ -17,33 +17,7 @@
 
 <body id="proyectos">
     <header class="conteniner">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active px-4 py-1" href="proyectos.html">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="cv">
-                    <a href="">Descarga mi CV<i class="fas fa-download"></i></a>
-                </div>
-            </div>
-        </nav>
+    <?php include_once("menu.php"); ?>
     </header>
 </body>
 <main class="container">
